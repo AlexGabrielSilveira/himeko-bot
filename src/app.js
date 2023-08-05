9b95ab7ffd11e8b651012c8886fe75719aa215be
AlexGabrielSilveira <alexgabrielyou@gmail.com>
2023-08-05 15:17:27 -0300

    agora os comandos sao setados em qualquer servidor

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 	try {
 		console.log(`Comandos rodando ${commands.length} !!.`);
 		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			Routes.applicationCommands(process.env.CLIENT_ID),
 			{ body: commands },
 		);
 
